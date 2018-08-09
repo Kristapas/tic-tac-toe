69a1292749df08ae88f9f85811605a7b59c601e3
unknown <sirgedas@email.com>
2018-08-09 20:21:05 +0300

    Added basic box click handler & couple utility functiones

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(() => {
+(function IIFE() {
 
   const gameData = [...Array(3)].map(item => [...Array(3)]);
 
@@ -6,15 +6,58 @@
   //kitais zodziais sukuriu tik tac toe lentele beveik
   const app = document.querySelector('#app');
 
+  const cross = '😈';
+  const circle = '😇';
+
+  const create = ({
+    tag,
+    classList,
+    textContent,
+    events = {}
+  }) => {
+    const element = document.createElement(tag);
+    element.classList = classList;
+    element.textContent = textContent;
+    Object.entries(events).forEach(([key, value]) => element.addEventListener(key, value));
+
+    return element;
+  }
+  const addGameData = (row, box) => {
+    gameData[row][box] = cross;
+    render();
+  };
+
+  const clearApp = () => {
+    const children = [...app.children];
+    children.forEach(child => app.removeChild(child));
+  }
+
+
   const render = () => {
+    clearApp();
+    const appContent = app.children;
     // console.log('I will render your tic tac toe');
-    gameData.forEach(rowData => {
-      const row = document.createElement('div');
-      row.classList.add('row');
-      rowData.forEach(boxData => {
-        const box = document.createElement('div');
-        box.classList.add('box');
-        box.textContent = boxData;
+    gameData.forEach((rowData, rowIndex) => {
+      const row = create({
+        tag: 'div',
+        classList: 'row'
+
+      });
+      // const row = document.createElement('div');
+      // row.classList.add('row');
+      rowData.forEach((boxData, boxIndex) => {
+        const box = create({
+          tag: 'div',
+          classList: 'box',
+          textContent: boxData,
+          events: {
+            click: () => addGameData(rowIndex, boxIndex)
+          },
+
+        });
+        // const box = document.createElement('div');
+        // box.classList.add('box');
+        // box.textContent = boxData;
         row.appendChild(box);
 
       });
@@ -30,15 +73,23 @@
   }) => {
     // alert('Lests Start');
     // console.log('event', target);
-    app.removeChild(target);
+    clearApp();
     render();
   };
 
   const initApp = () => {
-    const startButton = document.createElement('button');
-    startButton.textContent = 'Start';
-    startButton.classList.add('primary-button');
-    startButton.addEventListener('click', start);
+    const startButton = create({
+      tag: 'button',
+      classList: 'primary-button',
+      textContent: 'Start',
+      events: {
+        click: start,
+      }
+    });
+    // const startButton = document.createElement('button');
+    // startButton.textContent = 'Start';
+    // startButton.classList.add('primary-button');
+    // startButton.addEventListener('click', start);
     app.appendChild(startButton);
     // console.log('Init My App');
   };
